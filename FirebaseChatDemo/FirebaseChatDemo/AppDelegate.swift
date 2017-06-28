81d1095b1bbc4ca2fc3b5b341b78a38ab251dd76
sagar.thummar <sagar.thummar@solutionanalysts.com>
2017-06-28 14:55:16 +0530

    Added firebase pod in project.

--- FirebaseChatDemo/FirebaseChatDemo/AppDelegate.swift
+++ FirebaseChatDemo/FirebaseChatDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
-
+        FIRApp.configure()
+        
         return true
     }
 
